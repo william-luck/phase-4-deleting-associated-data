38ab587698d96267d6de6703dd923c5a5f7778cb
William Luck <luck.william.h@gmail.com>
2022-11-05 12:24:05 -0300

    commit tree 8a40306e3fa867242676623888f4345aad811665

--- app/models/dog_house.rb
+++ app/models/dog_house.rb
@@ -1,3 +1,4 @@
 class DogHouse < ApplicationRecord
-  has_many :reviews
+  # From the rails guide, the dependent is a supported option on had_many, and :destroy causes all the assocaited objects to also be destroyed?  
+  has_many :reviews, dependent: :destroy
 end
